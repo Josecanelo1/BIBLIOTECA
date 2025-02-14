44af92a11af5fe52ab26fbd48be8905e4487a956
Jose Mendoza <mendozamendoza1998@outlook.es>
2025-02-14 17:50:12 -0600

    Se creo la clase equiposbiblioteca

---
+++ BIBLIOTECA/Models/Equiposbiblioteca.cs
@@ -0,0 +1,15 @@
+﻿using BIBLIOTECA.Models.Tables;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace WebApiPractica.Models
+{
+    public class Equiposbiblioteca : DbContext
+    {
+        public Equiposbiblioteca(DbContextOptions<Equiposbiblioteca> options) : base(options)
+        {
+        }
+        public DbSet<Autor> Autor { get; set; }
+        public DbSet<Libro> Libro { get; set; }
+    }
+}
