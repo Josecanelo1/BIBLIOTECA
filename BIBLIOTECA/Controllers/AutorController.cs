a716512d08cf5e966f5cbb7a157c58717b850027
nelsonlopez500 <nelson.lopez@catolica.edu.sv>
2025-02-14 18:18:00 -0600

    Agregar controlador AutorController con endpoints para gestionar autores: obtener todos, por ID, crear, actualizar y eliminar.

--- BIBLIOTECA/Controllers/AutorController.cs
+++ BIBLIOTECA/Controllers/AutorController.cs
@@ -1,5 +1,7 @@
+using BIBLIOTECA.Models.Tables;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApiPractica.Models;
 
 namespace BIBLIOTECA.Controllers
 {
@@ -7,5 +9,80 @@ namespace BIBLIOTECA.Controllers
     [ApiController]
     public class AutorController : ControllerBase
     {
+        private readonly EquiposBiblioteca _equipoContexto;
+
+        public AutorController(EquiposBiblioteca equipoContexto)
+        {
+            _equipoContexto = equipoContexto;
+        }
+        /// <summary>
+        /// Endpoint que retorna el lista de autores existentes
+        /// </summary>
+        /// <returns>Lista de autores</returns>
+        [HttpGet]
+        [Route("GetAllAutores")]
+        public IActionResult Get()
+        {
+            var autores = (from a in _equipoContexto.Autor
+                        select a).ToList();
+            return Ok(autores);
+        }
+
+        /// <summary>
+        /// Endpoint que retorna un autor por su id
+        /// </summary>
+        
+        [HttpGet]
+        [Route("GetAutorById")]
+        public IActionResult GetAutorById(int id)
+        {
+            var autor = (from a in _equipoContexto.Autor
+                        where a.id == id
+                        select a).FirstOrDefault();
+            return Ok(autor);
+        }
+
+        /// <summary>
+        /// Endpoint que crea un autor
+        /// </summary>
+        /// 
+        [HttpPost]
+        [Route("CreateAutor")]
+        public IActionResult CreateAutor([FromBody] Autor autor)
+        {
+            _equipoContexto.Autor.Add(autor);
+            _equipoContexto.SaveChanges();
+            return Ok("Autor creado");
+        }
+
+        /// <summary>
+        /// Endpoint que actualiza un autor
+        /// </summary>
+        /// 
+        [HttpPut]
+        [Route("UpdateAutor")]
+        public IActionResult UpdateAutor([FromBody] Autor autor)
+        {
+            _equipoContexto.Autor.Update(autor);
+            _equipoContexto.SaveChanges();
+            return Ok("Autor actualizado");
+        }
+
+        /// <summary>
+        /// Endpoint que elimina un autor
+        /// </summary>
+        /// 
+        [HttpDelete]
+        [Route("DeleteAutor")]
+        
+        public IActionResult DeleteAutor(int id)
+        {
+            var autor = (from a in _equipoContexto.Autor
+                        where a.id == id
+                        select a).FirstOrDefault();
+            _equipoContexto.Autor.Remove(autor);
+            _equipoContexto.SaveChanges();
+            return Ok("Autor eliminado");
+        }
     }
 }
